a27be037874468f7f5c97501372a95e7dbab5931
Patrik Lythell <patrik@ohmy.nu>
2018-01-02 23:27:29 +0100

    add test for admin

---
+++ pages/admin/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <script src="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.js"></script>
+  </footer>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      link: [
+        {rel: 'stylesheet" href="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.css'}
+      ]
+    }
+  }
+}
+</script>
