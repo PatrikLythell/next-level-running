cf15cab900c978fc80f8b3d191ff79da3b543807
Patrik Lythell <patrik@ohmy.nu>
2018-09-15 23:52:04 +0200

    meta fix to check for meta before build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,17 +12,17 @@ const TemplateWrapper = ({ children, header, meta }) => {
   return (
     <div className="font-sans text-md sm:text-lg leading-tight">
       <Helmet
-        title={meta.title || 'Next Level Running - Träna som Eliten'}
+        title={(meta && meta.title) ? meta.title : 'Next Level Running - Träna som Eliten'}
         link={[
           { rel: "icon", type:"image/png", sizes:"192x192", href:"/img/favicon.png" }
         ]}
         meta={[
-          { name: "description", content: meta.description || "Next Level Running erbjuder skräddarsydd, effektiv och målinriktad löpcoaching i nära samarbete med Tor Pöllänen - Löpcoach och fd Landslagslöpare." },
+          { name: "description", content: (meta && meta.description) ? meta.description : "Next Level Running erbjuder skräddarsydd, effektiv och målinriktad löpcoaching i nära samarbete med Tor Pöllänen - Löpcoach och fd Landslagslöpare." },
           { name: "og:url", content: "https://nextlevelrunning.se" },
           { name: "og:type", content: "website" },
-          { name: "og:title", content: meta.title || "Träna som eliten – Ta din löpning till nästa nivå" },
-          { name: "og:description", content: meta.description || "Next Level Running erbjuder skräddarsydd, effektiv och målinriktad löpcoaching i nära samarbete med Tor Pöllänen - Löpcoach och fd Landslagslöpare." },
-          { name: "og:image", content: `https://nextlevelrunning.se${meta.ogImage}` || "https://nextlevelrunning.se/img/og-image.png" },
+          { name: "og:title", content: (meta && meta.title) ? meta.title : "Träna som eliten – Ta din löpning till nästa nivå" },
+          { name: "og:description", content: (meta && meta.description) ? meta.description : "Next Level Running erbjuder skräddarsydd, effektiv och målinriktad löpcoaching i nära samarbete med Tor Pöllänen - Löpcoach och fd Landslagslöpare." },
+          { name: "og:image", content: (meta && meta.ogImage) ? `https://nextlevelrunning.se${meta.ogImage}` : "https://nextlevelrunning.se/img/og-image.png" },
 
         ]}
       />
